1b86907a77a88ee98daf83ace32aa9b308d2bd2d
Marshal Pro <marshal.programmer@gmail.com>
2021-02-28 19:35:37 +0300

    The robots collection is linked to Card component. 6 members of my family are successfully showing now.

---
+++ robofriends/src/robots.js
@@ -0,0 +1,44 @@
+export const robots = [
+	{
+		id: 1,
+		name: 'Mustafa Karrokhi',
+		username: 'm.bukhari',
+		email: 'm.bukhari@albaladalameen.com.sa'
+	},
+	{
+		id: 2,
+		name: 'Afaf Najibullah',
+		username: 'fafa',
+		email: 'fafa@gmail.com'
+	},
+	{
+		id: 3,
+		name: 'Yahya Karrokhi',
+		username: 'Yaya',
+		email: 'yahya.karrokhi@gmail.com'
+	},
+	{
+		id: 4,
+		name: 'Yousuf Karrokhi',
+		username: 'Yoyo',
+		email: 'yousuf.karrokhi@gmail.com'
+	},
+	{
+		id: 5,
+		name: 'Eisa Karrokhi',
+		username: 'eisa',
+		email: 'eisa.karrokhi@gmail.com'
+	},
+	{
+		id: 6,
+		name: 'Sara Karrokhi',
+		username: 'Sara',
+		email: 'sara.karrokhi@gmail.com'
+	},
+	{
+		id: 7,
+		name: 'Abdur Rahman Karrokhi',
+		username: 'AbdurRahman',
+		email: 'ar.karrokhi@gmail.com'
+	}
+];
