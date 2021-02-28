78952c2cbde2e5f1f2d4e65263eab1f2493805bb
Marshal Pro <marshal.programmer@gmail.com>
2021-02-28 20:06:34 +0300

    Cardlist component created to hold all robot cards. It is hard coded write now.

--- robofriends/src/Card.js
+++ robofriends/src/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Card =(props, { id, name, email}) => {
+const Card =({ id, name, email}) => {
 	return (
 		<div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-			<img alt='robots' src={`https://robohash.org/${props.id}`}/>
+			<img alt='robots' src={`https://robohash.org/${id}`}/>
 			<div>
-				<h2>{props.name}</h2>
-				<p>{props.email}</p>
+				<h2>{name}</h2>
+				<p>{email}</p>
 			</div>
 		</div>
 	)
