15d70ed27c70d7d133d339afc08d928e6a3243e7
Marshal Pro <marshal.programmer@gmail.com>
2022-02-16 23:15:50 +0300

    robots array linked with the data

--- robofriends/src/robots.js
+++ robofriends/src/robots.js
@@ -2,42 +2,49 @@ export const robots = [
 	{
 		id: 1,
 		name: 'Mustafa Karrokhi',
+		aname: 'مصطفى كروخي',
 		username: 'm.bukhari',
 		email: 'm.bukhari@albaladalameen.com.sa'
 	},
 	{
 		id: 2,
 		name: 'Afaf Najibullah',
+		aname: 'عفاف نجيب الله قاري',
 		username: 'fafa',
 		email: 'fafa@gmail.com'
 	},
 	{
 		id: 3,
 		name: 'Yahya Karrokhi',
+		aname: 'يحيى مصطفى كروخي',
 		username: 'Yaya',
 		email: 'yahya.karrokhi@gmail.com'
 	},
 	{
 		id: 4,
 		name: 'Yousuf Karrokhi',
+		aname: 'يوسف مصطفى كروخي',
 		username: 'Yoyo',
 		email: 'yousuf.karrokhi@gmail.com'
 	},
 	{
 		id: 5,
 		name: 'Eisa Karrokhi',
+		aname: 'عيسى مصطفى كروخي',
 		username: 'eisa',
 		email: 'eisa.karrokhi@gmail.com'
 	},
 	{
 		id: 6,
 		name: 'Sara Karrokhi',
+		aname: 'سارة مصطفى كروخي',
 		username: 'Sara',
 		email: 'sara.karrokhi@gmail.com'
 	},
 	{
 		id: 7,
 		name: 'Abdur Rahman Karrokhi',
+		aname: 'عبدالرحمن مصطفى كروخي',
 		username: 'AbdurRahman',
 		email: 'ar.karrokhi@gmail.com'
 	}
