43ff95a29b42d91b417dda1e6d448d8a6199ec50
MKarrokhi <mustafa.karrokhi@gmail.com>
2022-02-22 19:33:08 +0300

    Searchbox is now sending the value to console.log

---
+++ robofriends/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import {robots} from './robots';
+
+
+class App extends Component {
+	constructor(){
+		super();
+		this.state = {
+			robots: robots,
+			searchfield: ''
+		}
+	}
+
+	onSearchChange(event){
+		console.log(event.target.value);
+		// const filteredRobots = this.state.robots.filter(robot => {
+		// 	return robot.aname.includes(this.state.searchfield);
+		// })
+	}
+
+	render () {
+		return (
+			<div className='tc'>
+				<h1>عائلة مصطفى كروخي </h1>
+				<SearchBox searchChange={this.onSearchChange}/>
+				<CardList robots={this.state.robots}/>
+			</div>
+		);
+	}
+}
+
+export default App;
